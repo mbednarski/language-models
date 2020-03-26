62c186603833e25023e384afa6b55f956ed6e686
mbednarski <mateusz.bednarski@windowslive.com>
2020-03-26 13:29:49 +0100

    Dino dataset

--- src/language_models/dataset/dinosaurs.py
+++ src/language_models/dataset/dinosaurs.py
@@ -1,58 +1,60 @@
 import torch
 from torch.utils.data import Dataset
+from pathlib import Path
+
 
 class CharacterVocabulary:
-  SOS_TOKEN = '^'
-  EOS_TOKEN = '$'
+    SOS_TOKEN = "^"
+    EOS_TOKEN = "$"
 
-  def __init__(self):
-    self.idx2char = list(self.SOS_TOKEN + self.EOS_TOKEN + 'abcdefghijklmnopqrstuvwxyz')
-    self.char2idx = {c:i for i, c in enumerate(self.idx2char)}
+    def __init__(self):
+        self.idx2char = list(
+            self.SOS_TOKEN + self.EOS_TOKEN + "abcdefghijklmnopqrstuvwxyz"
+        )
+        self.char2idx = {c: i for i, c in enumerate(self.idx2char)}
 
-  def __len__(self):
-    return len(self.idx2char)
+    def __len__(self):
+        return len(self.idx2char)
 
-  def encode(self, s:str, include_special_tokens=False) -> torch.LongTensor:
-    if include_special_tokens:
-      s = self.SOS_TOKEN + s + self.EOS_TOKEN
-    values = [self.char2idx[c] for c in s]
-    return torch.LongTensor(values)
-  
-  def decode(self, t:torch.LongTensor) -> str:
-    decoded = [self.idx2char[i] for i in t]
-    return ''.join(decoded)
+    def get_vocab_size(self):
+        return len(self.char2idx)
 
-class DinoDataset(Dataset):
-  def __init__(self):
-    lines = []
-    with open('data/raw/dinosaurs.txt', 'rt') as f:
-      lines = f.readlines()
-    self.lines = [SOS_TOKEN +  l.lower().strip() + EOS_TOKEN  for l in lines]
+    def encode(self, s: str, include_special_tokens=False) -> torch.LongTensor:
+        if include_special_tokens:
+            s = self.SOS_TOKEN + s + self.EOS_TOKEN
+        values = [self.char2idx[c] for c in s]
+        return torch.LongTensor(values)
 
-    self.idx2char = list(SOS_TOKEN + EOS_TOKEN + 'abcdefghijklmnopqrstuvwxyz')
-    self.char2idx = {c:i for i, c in enumerate(self.idx2char)}
+    def decode(self, t: torch.LongTensor) -> str:
+        decoded = [self.idx2char[i] for i in t]
+        return "".join(decoded)
 
-  def __len__(self):
-    return len(self.lines)
 
-  def __getitem__(self, idx):
-    # TODO: encode to tensor only once and return two slices of it as x and y
-    x = self.lines[idx][:-1]
-    y = self.lines[idx][1:]
+class WordDataset(Dataset):
+    def __init__(self, vocab=None, dset_path: Path = None):
+        if vocab is None:
+            self.vocab = CharacterVocabulary()
+        else:
+            self.vocab = vocab
 
-    x = torch.LongTensor([self.char2idx[xx] for xx in x])
-    y = torch.LongTensor([self.char2idx[yy] for yy in y])
+        if dset_path is None:
+            dset_path = Path("data/raw/dinosaurs.txt")
+        else:
+            dset_path = dset_path
 
-    return x, y
+        lines = []
+        with dset_path.open("rt") as f:
+            lines = [l.strip() for l in f.readlines()]
+        self.lines = [l for l in lines if len(l) > 0]
 
-  def get_vocab_size(self):
-      return len(self.char2idx)
+    def __len__(self):
+        return len(self.lines)
 
-  def decode(self, encoded:torch.Tensor) -> str:
-    decoded = ''.join([self.idx2char[idx] for idx in encoded])
-    return decoded
+    def __getitem__(self, idx):
+        x = self.lines[idx]
+        y = self.lines[idx]
 
-  def encode(self, s:str) -> str:
-    encoded = torch.LongTensor([self.char2idx[xx] for xx in s])
+        x = self.vocab.encode(x, include_special_tokens=True)[:-1]
+        y = self.vocab.encode(y, include_special_tokens=True)[1:]
 
-    return encoded
+        return x, y
--- tests/dataset/test_dinosaurs.py
+++ tests/dataset/test_dinosaurs.py
@@ -1,30 +1,42 @@
-from language_models.dataset.dinosaurs import CharacterVocabulary
+from language_models.dataset.dinosaurs import CharacterVocabulary, WordDataset
 
 import torch
 from torch.testing import assert_allclose
 
+
 def test_encode():
     vocab = CharacterVocabulary()
 
-    text = 'trex'
+    text = "trex"
     encoded = vocab.encode(text)
 
     assert encoded.shape == (4,)
-    assert_allclose(encoded, torch.LongTensor([21,19,6,25]))
+    assert_allclose(encoded, torch.LongTensor([21, 19, 6, 25]))
+
 
 def test_encode_special():
     vocab = CharacterVocabulary()
 
-    text = 'trex'
+    text = "trex"
     encoded = vocab.encode(text, include_special_tokens=True)
 
     assert encoded.shape == (6,)
-    assert_allclose(torch.LongTensor([0,21,19,6,25,1]), encoded)
+    assert_allclose(torch.LongTensor([0, 21, 19, 6, 25, 1]), encoded)
+
 
 def test_decode():
     vocab = CharacterVocabulary()
 
-    encoded = torch.LongTensor([0,21,19,6,25,1])
+    encoded = torch.LongTensor([0, 21, 19, 6, 25, 1])
     decoded = vocab.decode(encoded)
 
     assert vocab.SOS_TOKEN + "trex" + vocab.EOS_TOKEN == decoded
+
+
+def test_dataset():
+    dset = WordDataset()
+    vocab = dset.vocab
+
+    x, y = dset[12]
+    assert vocab.decode(x) == '^acristavus'
+    assert vocab.decode(y) == 'acristavus$'
