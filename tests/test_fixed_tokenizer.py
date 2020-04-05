8edb817e221a859f6c81456e80846697f380ed37
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2020-04-05 15:40:37 +0200

    Added FixedTokenizer

---
+++ tests/test_fixed_tokenizer.py
@@ -0,0 +1,32 @@
+from hypothesis import given, assume
+from hypothesis.strategies import text
+
+from language_models.tokenizer import FixedTokenizer
+
+
+def test_defaults():
+    tok = FixedTokenizer()
+
+    assert 30 == tok.get_vocab_size()
+    assert 0 == tok.tok2idx[tok.PAD_TOKEN]
+    assert 1 == tok.tok2idx[tok.SOS_TOKEN]
+    assert 2 == tok.tok2idx[tok.EOS_TOKEN]
+
+@given(text(alphabet=FixedTokenizer.ALLOWED_TOKENS))
+def test_encode_decode(s:str):
+    assume(len(s) > 0)
+    tok = FixedTokenizer()
+
+    encoded = tok.encode(s, include_special_tokens=False)
+    decoded = tok.decode(encoded)
+
+    assert decoded == s
+
+@given(text(alphabet=FixedTokenizer.ALLOWED_TOKENS))
+def test_encode_decode_with_special(s:str):
+    tok = FixedTokenizer()
+
+    encoded = tok.encode(s, include_special_tokens=True)
+    decoded = tok.decode(encoded)
+
+    assert decoded == tok.SOS_TOKEN + s + tok.EOS_TOKEN
